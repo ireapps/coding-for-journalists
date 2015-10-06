f15ebcedb2aae6e08456fc5289dd8c76981a6d7d
Alex Richards <richards.alex@gmail.com>
2015-10-06 18:46:42 -0500

    additions and some PEP8 cleanup

---
+++ out_of_dbs/to_json.py
@@ -0,0 +1,7 @@
+# Now that we've processed JSON, moved into into a database an set up some helpful 
+# prewritten queries for new records, we're going to talk about the flip side of that: 
+# exporting data to JSON for use elsewhere.
+
+import sqlite3
+import json
+
