cf0704f02de3559720a08439d674403bc5351b72
Alex Richards <richards.alex@gmail.com>
2015-10-28 23:40:44 -0500

    some pep8 cleanup for new stuff + a file rename

--- cleaning/names.py
+++ cleaning/names.py
@@ -16,7 +16,7 @@ board_links = soup.find_all('a', href=re.compile('cid='))
 
 # Quick check to make sure we're getting all of our links before we move on
 for link in board_links:
-	print link.text
+    print link.text
 print 'Total number of boards: '+str(len(board_links))
 
 # Let's look at the URL string
@@ -30,13 +30,13 @@ print board_links[1].get('href')
 board_pages = []
 
 for link in board_links:
-	board_name = link.string
-	board_url = link.get('href')
-	# We only need the number at the end; we can split the URL and grab it
-	board_code = board_url.split('?cid=')[1]
-	tup = (board_name, board_code)
-	board_pages.append(tup)
-	
+    board_name = link.string
+    board_url = link.get('href')
+    # We only need the number at the end; we can split the URL and grab it
+    board_code = board_url.split('?cid=')[1]
+    tup = (board_name, board_code)
+    board_pages.append(tup)
+
 board_url = 'https://webapps1.cityofchicago.org/moboco/org/cityofchicago/moboc/controller/view/searchBoard.do?cid='
 
 # Just as a test, let's look at the first one to see what we're dealing with.
@@ -68,50 +68,50 @@ clean_name = ' '.join(name_text.split())
 
 name_list = []
 for name_part in first_row.td.find_all('label'):
-	name_list.append(name_part.text)
+    name_list.append(name_part.text)
 
 # Now that we see how this works, we can write a full run. We'll feed
 # requests.get a page, parse it with BeautifulSoup, and write it to CSV.
 
 # We can use the 'with' syntax here to open the file
 with open('./boards.csv', 'wb') as csv_outfile:
-	writer = csv.writer(csv_outfile)
-	writer.writerow(['BOARD', 'FULL_NAME', 'TITLE', 'FIRST_NAME', 'MIDDLE', 'LASTNAME', 'SUFFIX', 'TERM', 'APPOINTER'])
-	for board_page in board_pages:
-		# We can basically use what we've done above to zero in on the table
-		page = requests.get(board_url+board_page[1])
-		soup = BeautifulSoup(page.content, 'html.parser')
-		page_tables = soup.find_all('table', {'class': 'resultTable'})
-		member_table = page_tables[1].find_all('tr')
-		# Another for loop to move inside each row (except the header)
-		for row in member_table[1:]:
-			cells = row.find_all('td')
-			name_cell = cells[0]
-			term_cell = cells[1]
-			appt_cell = cells[2]
-			full_name = ' '.join(name_cell.text.split())
-			# If it's a vacancy, let's not go through the trouble of splitting.
-			if full_name == 'Vacancy':
-				title, first_name, mi, last_name, suffix = '', '', '', '', ''
-			else:
-				# Slot the different components here
-				name_parts = []
-				for part in name_cell.find_all('label'):
-					name_parts.append(part.text)
-				title = name_parts[0]
-				first_name = name_parts[1]
-				mi = name_parts[2]
-				# Last name is currently including suffixes like Jr, Sr, III, etc.
-				# We can look for a comma that denotes the suffix.
-				if name_parts[3].find(',') == -1:
-					last_name = name_parts[3]
-					suffix = ''
-				else:
-					last_split = name_parts[3].split(', ')
-					last_name = last_split[0]
-					suffix = last_split[1]
-			term = term_cell.text.strip()
-			appt = appt_cell.text.strip()
-			writer.writerow([board_page[0], full_name, title, first_name, mi, last_name, suffix, term, appt])
-		print 'Fetched and wrote '+board_page[0]
-		time.sleep(2)
+    writer = csv.writer(csv_outfile)
+    writer.writerow(['BOARD', 'FULL_NAME', 'TITLE', 'FIRST_NAME', 'MIDDLE', 'LASTNAME', 'SUFFIX', 'TERM', 'APPOINTER'])
+    for board_page in board_pages:
+        # We can basically use what we've done above to zero in on the table
+        page = requests.get(board_url+board_page[1])
+        soup = BeautifulSoup(page.content, 'html.parser')
+        page_tables = soup.find_all('table', {'class': 'resultTable'})
+        member_table = page_tables[1].find_all('tr')
+        # Another for loop to move inside each row (except the header)
+        for row in member_table[1:]:
+            cells = row.find_all('td')
+            name_cell = cells[0]
+            term_cell = cells[1]
+            appt_cell = cells[2]
+            full_name = ' '.join(name_cell.text.split())
+            # If it's a vacancy, let's not go through the trouble of splitting.
+            if full_name == 'Vacancy':
+                title, first_name, mi, last_name, suffix = '', '', '', '', ''
+            else:
+                # Slot the different components here
+                name_parts = []
+                for part in name_cell.find_all('label'):
+                    name_parts.append(part.text)
+                title = name_parts[0]
+                first_name = name_parts[1]
+                mi = name_parts[2]
+                # Last name is currently including suffixes like Jr, Sr, III, etc.
+                # We can look for a comma that denotes the suffix.
+                if name_parts[3].find(',') == -1:
+                    last_name = name_parts[3]
+                    suffix = ''
+                else:
+                    last_split = name_parts[3].split(', ')
+                    last_name = last_split[0]
+                    suffix = last_split[1]
+            term = term_cell.text.strip()
+            appt = appt_cell.text.strip()
+            writer.writerow([board_page[0], full_name, title, first_name, mi, last_name, suffix, term, appt])
+        print 'Fetched and wrote '+board_page[0]
+        time.sleep(2)
--- other_scrapes/other_scrapes.py
+++ other_scrapes/other_scrapes.py
@@ -3,6 +3,7 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import time
 
 # We're not always going to be diving into a table or other information tied
 # up in HTML. Sometimes our target is a file or set of files; we can write the content
@@ -32,7 +33,7 @@ pdf_links = soup.find_all(href=re.compile('.pdf'))
 # of just <list item>.
 
 for link in enumerate(pdf_links):
-	print '{0}: {1}'.format(link[0]+1, link[1].string)
+    print '{0}: {1}'.format(link[0]+1, link[1].string)
 
 # Make an empty list to hold the URLs we're going to pull out of all these links.
 
@@ -41,10 +42,10 @@ pdf_urls = []
 # Loop through and grab the URLs.
 
 for link in pdf_links:
-	pdf_urls.append(link['href'])
+    pdf_urls.append(link['href'])
 
 # Now we have a list of PDF files to grab from the page. We're going to write
-# the content in each to a file, and we can use information we've gleaned 
+# the content in each to a file, and we can use information we've gleaned
 # from the file or link to name the files that we're writing to disk.
 
 # Let's use a dict instead; the key will be the link text and the value will
@@ -53,38 +54,32 @@ for link in pdf_links:
 pdf_urls_dict = {}
 
 for link in pdf_links:
-	pdf_urls_dict[link.string] = link['href']
-	
+    pdf_urls_dict[link.string] = link['href']
+
 # We can get the original file name out of the URL by splitting URL at '/' and
 # grabbing the last item in the list.
 
 name_ex = pdf_urls_dict['District 010'].split('/')[-1]
 print name_ex
-	
+
 # Since the original file names don't contain any information about the District
 # or Area, let's add that in.
 
 # Also, let's make sure we're not bombarding the site with a ton of requests at
 # the same time; we need to briefly pause between downloads.
 
-import time 
-
 # We'll pass the URL to a variable, get it with requests, do a little tinkering
 # with the PDF name and link name to remove spaces (and the URL equivalent),
 # then write them to a directory.
 
 for url in pdf_urls_dict:
-	pdf = pdf_urls_dict[url]
-	file = requests.get(pdf)
-	loc_name = url.replace(' ', '')
-	pdf_name = pdf.split('/')[-1].replace('%20', '')
-	file_name = '{0}-{1}'.format(loc_name, pdf_name)
-	directory = './pdfs/'
-	with open(directory+file_name, 'wb') as outfile:
-		outfile.write(file.content)
-	print 'Grabbed '+loc_name
-	time.sleep(1)
-	
-
-
-	
+    pdf = pdf_urls_dict[url]
+    file = requests.get(pdf)
+    loc_name = url.replace(' ', '')
+    pdf_name = pdf.split('/')[-1].replace('%20', '')
+    file_name = '{0}-{1}'.format(loc_name, pdf_name)
+    directory = './pdfs/'
+    with open(directory+file_name, 'wb') as outfile:
+        outfile.write(file.content)
+    print 'Grabbed '+loc_name
+    time.sleep(1)
